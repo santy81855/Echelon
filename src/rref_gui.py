43368af0ffa55ba2a1b543f62976c6837fc5ecf8
santy81855 <mynameissantiagogarcia@gmail.com>
2021-09-24 12:08:32 -0400

    added basic framework for the gui, and it has 2 different windows now

--- src/rref_gui.py
+++ src/rref_gui.py
@@ -3,6 +3,8 @@ from PyQt5 import QtWidgets, QtCore, QtGui
 from PyQt5.QtWidgets import QDialog, QApplication, QWidget, QFileDialog, QInputDialog, QLineEdit, QPushButton
 from fractions import Fraction
 import copy
+import ctypes
+import sys
 
 # this sets the icon as your taskbar icon
 myappid = 'Row Reducer'
@@ -23,7 +25,7 @@ success = ctypes.windll.user32.SetProcessDPIAware()
 
 # global variables
 input_matrix = []
-want_fractions = 0
+want_fractions = True
 row_number = 0
 column_number = 0
 
@@ -31,16 +33,62 @@ class WelcomeScreen(QDialog):
     def __init__(self):
         super(WelcomeScreen, self).__init__()
         loadUi("welcomescreen.ui", self)
-        self.start_button.clicked.connect(self.startProgram)
+        self.start_button.clicked.connect(self.start_program)
     # launch the month selector widget when the start button is pressed
-    def startProgram(self):
-        month = MonthScreen()
-        widget.addWidget(month)
+    def start_program(self):
+        get_input = Input()
+        widget.addWidget(get_input)
         widget.setCurrentIndex(widget.currentIndex()+1)
+
+class Input(QDialog):
+    def __init__(self):
+        super(Input, self).__init__()
+        loadUi("input.ui", self)
+        self.next_button.clicked.connect(self.get_matrix)
+        self.fractions_check.stateChanged.connect(self.check_fractions)
+        self.decimals_check.stateChanged.connect(self.check_decimals)
+    
+    def check_fractions(self):
+        global want_fractions
+        want_fractions = True
+    def check_decimals(self):
+        global want_fractions
+        want_fractions = False
+    
+    def get_matrix(self):
+        # store the info they just gave
+        global row_number
+        global column_number
+        global want_fractions
+
+        # need to make sure they fully input all of the data before pressing next so the program doesn't crash
+        errors = 0
+        # start to take in the input
+        # don't proceed if they leave a line blank which is optional
+        if len(self.input_row.text()) == 0 or str(self.input_row.text()) == '0':
+            self.error_1.setText("Enter a valid row number.")
+            errors += 1
+        else:
+            self.error_1.setText('')
+            row_number = int(self.input_row.text())
+
+        if len(self.input_column.text()) == 0 or str(self.input_row.text()) == '0':
+            self.error_2.setText("Enter a valid column number.")
+            errors += 1
+        else:
+            self.error_2.setText('')
+            column_number = int(self.input_column.text())    
+
+        #if errors == 0:
+         #   matrix_screen = Matrix()
+          #  widget.addWidget(matrix_screen)
+           # widget.setCurrentIndex(widget.currentIndex()+1)    
+        
+        
 
 # main code
 app = QApplication(sys.argv)
-app.setWindowIcon(QtGui.QIcon('logo.ico')) // sets the logo
+app.setWindowIcon(QtGui.QIcon('logo.ico')) # sets the logo
 welcome = WelcomeScreen()
 widget = QtWidgets.QStackedWidget()
 widget.addWidget(welcome)
